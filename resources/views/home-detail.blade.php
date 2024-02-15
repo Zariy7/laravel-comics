1fe54436fb95cef50e5e09a841448f7d9da8f411
Zariy7 <nazariy.hereha@gmail.com>
2024-02-15 08:01:35 +0100

    detail stylization

--- resources/views/home-detail.blade.php
+++ resources/views/home-detail.blade.php
@@ -1,16 +1,29 @@
 @extends('layout.app')
 
 @section('content')
-<img src="{{$focus['thumb']}}" alt="">
+
 <div class="container">
+    <div class="w-25">
+        <img src="{{$focus['thumb']}}" class="w-50">
+    </div>
     <div class="row">
-        <div class="col-12">
+        <div class="col-8">
             <h1>
                 {{$focus['title']}}
             </h1>
 
-            <div>
-                {{$focus['price']}}
+            <div class="d-flex justify-content-between bg-success text-white">
+                <div class="d-flex justify-content-between w-75 px-2">
+                    <div>
+                        U.S. Price: {{$focus['price']}}
+                    </div>
+                    <div>
+                        AVAILABLE
+                    </div>
+                </div>
+                <div class="btn btn-success">
+                    Check Availability
+                </div>
             </div>
 
             <p>
@@ -22,21 +35,59 @@
             <h3>
                 Talent
             </h3>
+            <hr>
+            <div class="d-flex justify-content-between">
+                <div>
+                    Art By: 
+                </div>
+                <div>
+                    Lorem Ipsum
+                </div>
+            </div>
+            <hr>
+            <div class="d-flex justify-content-between">
+                <div>
+                    Written By: 
+                </div>
+                <div>
+                    Dolor Sit Amet
+                </div>
+            </div>
+            <hr>
         </div>
 
         <div class="col-6">
             <h3>
                 Specs
             </h3>
-            <div>
-                {{$focus['series']}}
+            <hr>
+            <div class="d-flex justify-content-between">
+                <div>
+                    Series: 
+                </div>
+                <div>
+                    {{$focus['series']}}
+                </div>
             </div>
-            <div>
-                {{$focus['price']}}
+            <hr>
+            <div class="d-flex justify-content-between">
+                <div>
+                    U.S. Price: 
+                </div>
+                <div>
+                    {{$focus['price']}}
+                </div>
             </div>
-            <div>
-                {{$focus['sale_date']}}
+            <hr>
+            <div class="d-flex justify-content-between">
+                <div>
+                    On Sale Date:     
+                </div>
+                <div>
+                    {{$focus['sale_date']}}
+                </div>
             </div>
+            <hr>
         </div>
     </div>
 </div>
